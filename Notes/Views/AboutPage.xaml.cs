a21870345f634d133d2a9b6d7b423550cc52fc73
aashraybavisa <aashray1042@gmail.com>
2025-01-05 09:54:26 -0500

    feat: map implementation in mac with basic controls

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -1,3 +1,8 @@
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+using Map = Microsoft.Maui.Controls.Maps.Map;
+using Maps = Microsoft.Maui.Maps;
+
 namespace Notes.Views;
 
 public partial class AboutPage : ContentPage
@@ -5,15 +10,33 @@ public partial class AboutPage : ContentPage
     public AboutPage()
     {
         InitializeComponent();
+        // Map map = new Map
+        // {
+        //     MapType = Maps.MapType.Satellite,
+        //     IsTrafficEnabled = true,
+        //     IsShowingUser = true
+        // };
+        Location location = new Location(36.9628066, -122.0194722);
+        MapSpan mapSpan = new MapSpan(location, 0.01, 0.01);
+        Map map = new Map(mapSpan);
+        Pin pin = new Pin
+        {
+            Label = "Santa Cruz",
+            Address = "The city with a boardwalk",
+            Type = PinType.Place,
+            Location = new Location(36.9628066, -122.0194722)
+        };
+        map.Pins.Add(pin);
+        Content = map;
     }
 
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        // Navigate to the specified URL in the system browser.
-        if (BindingContext is Models.About about)
-        {
-            // Navigate to the specified URL in the system browser.
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
-    }
+    // private async void LearnMore_Clicked(object sender, EventArgs e)
+    // {
+    //     // Navigate to the specified URL in the system browser.
+    //     if (BindingContext is Models.About about)
+    //     {
+    //         // Navigate to the specified URL in the system browser.
+    //         await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+    //     }
+    // }
 }
